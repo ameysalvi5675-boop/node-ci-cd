6fc285311d444ca3c53774bf4ed5f38b80595b24
ameysalvi5675-boop <ameypro157@gmail.com>
2026-04-19 13:56:24 +0530

    Moved to Helm Deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello DevOps 🚀");
+  res.send("Helm CI/CD working 🚀");
 });
 
 app.listen(3000, () => {
